b19b69dfe06d636a2c4b3ee437a913ce2afbb752
0x0006e <yaoxc07@gmail.com>
2020-06-04 13:25:32 +0800

    feat: add some options

--- bin/index.ts
+++ bin/index.ts
@@ -1,2 +1,18 @@
 #!/usr/bin/env node
-console.log("Hello Cli");
+import { Command } from "commander";
+import PackageJson from "../package.json";
+
+const program = new Command();
+const { version } = PackageJson;
+
+program
+  .version(version, "-v, --vers", "output the current version")
+  .option("-d, --debug", "output extra debugging")
+  .option("-s, --small", "small pizza size")
+  .option("-p, --pizza-type <type>", "flavour of pizza");
+
+program.parse(process.argv);
+
+if (program.debug) console.log(program.opts());
+if (program.small) console.log("- small pizza size");
+if (program.pizzaType) console.log(`- ${program.pizzaType}`);
--- build/bin/index.js
+++ build/bin/index.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var commander_1 = require("commander");
+var package_json_1 = __importDefault(require("../package.json"));
 var program = new commander_1.Command();
-var version = "0.0.1";
-program.version(version);
+var version = package_json_1.default.version;
 program
+    .version(version, "-v, --vers", "output the current version")
     .option("-d, --debug", "output extra debugging")
     .option("-s, --small", "small pizza size")
     .option("-p, --pizza-type <type>", "flavour of pizza");
